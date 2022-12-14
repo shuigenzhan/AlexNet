ff2a4fe480eeeda062d3778f3298a6742825fe35
shuigenzhan <814974481@qq.com>
2022-12-14 17:51:51 +0800

    modify train.py

--- README.md
+++
@@ -1,3 +0,0 @@
-##  数据集下载链接
-	
-	[flower_dataset](http://download.tensorflow.org/example_images/flower_photos.tgz)
--- train.py
+++ train.py
@@ -10,49 +10,51 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 from torchvision import datasets
 from torch.utils.data import DataLoader
+from torchvision.transforms.functional import InterpolationMode
 from model.model import AlexNet
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device: ', device)
     warnings.filterwarnings('ignore')
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-drop', '--dropout', type=float, default=0.5, help="Dropout rate.")
-    parser.add_argument('-lr', type=float, default=0.0002, help="Initial learning rate.")
-    parser.add_argument('-e', '--epochs', type=int, default=10, help="Number of epochs to train.")
-    parser.add_argument('-bs', '--batch_size', type=int, default=32, help="Batch size.")
-    parser.add_argument('-train_fn', type=str, default='/Users/zsg/alexnet/pytorch_alexnet/flower_data/train/', help='train dataset')
-    parser.add_argument('-valid_fn', type=str, default='/Users/zsg/alexnet/pytorch_alexnet/flower_data/val/', help='valid dataset')
-    parser.add_argument('-nc', '--num_class', type=int, default=1000,  help="number class.")
+    parser.add_argument('--lr', type=float, default=0.01, help="Initial learning rate.")
+    parser.add_argument('-e', '--epochs', type=int, default=20, help="Number of epochs to train.")
+    parser.add_argument('-bs', '--batch_size', type=int, default=256, help="Batch size.")
+    parser.add_argument('--train_fn', type=str, default='./CIFAR10/', help='train dataset')
+    parser.add_argument('--test_fn', type=str, default='./CIFAR10/', help='test dataset')
+    parser.add_argument('-nc', '--num_class', type=int, default=1000, help="number class.")
+    parser.add_argument('--valid_ration', type=float, default=0.1, help="ration of valid set")
     args = parser.parse_args()
 
     writer = SummaryWriter('./log/')
 
-    data_transform = {
-        'train': transforms.Compose([transforms.RandomResizedCrop(227),
-                                     transforms.RandomHorizontalFlip(),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-        'val': transforms.Compose([transforms.Resize((227, 227)),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    }
+    data_transform = transforms.Compose(
+        [transforms.Resize((227, 227), interpolation=InterpolationMode.BICUBIC), transforms.RandomHorizontalFlip(),
+         transforms.ToTensor(), transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.247, 0.2435, 0.2616))])
 
     batch_size = args.batch_size
-    train_set = datasets.ImageFolder(root=args.train_fn, transform=data_transform['train'])
-    val_set = datasets.ImageFolder(root=args.valid_fn, transform=data_transform['val'])
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=0)
+    VAL_RATION = args.valid_ration
+    dataset = datasets.CIFAR10(root=args.train_fn, train=True, transform=data_transform, download=True)
+    train_size = int((1 - VAL_RATION) * len(dataset))
+    valid_size = len(dataset) - train_size
+    train_set, valid_set = torch.utils.data.random_split(dataset, [train_size, valid_size])
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_set, batch_size=args.batch_size, shuffle=True)
 
-    network = AlexNet(num_class=args.num_class, dropout=args.dropout)
+    network = AlexNet(num_class=args.num_class, dropout=args.dropout).to(device)
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(params=network.parameters(), lr=0.0002)
-    epochs = args.epochs
+    optimizer = torch.optim.SGD(params=network.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.0005)
     train_step = len(train_loader)
-    val_num = len(val_set)
+    valid_num = len(valid_set)
     best_acc = 0.0
-    for epoch in range(epochs):
+
+    for epoch in range(args.epochs):
         network.train()
         step = 1
         for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             y_hat = network(images)
             loss = loss_fn(y_hat, labels)
@@ -61,21 +63,23 @@ if __name__ == '__main__':
             writer.add_scalar('loss', loss, step)
             writer.add_images('images', images, step)
             writer.add_graph(network, images)
-            print(f'Epoch: [{epoch + 1} / {epochs}], step: [{step} / {train_step}], loss: [{loss:.4f}]')
+            print(f'Epoch: [{epoch + 1} / {args.epochs}], step: [{step} / {train_step}], loss: [{loss:.4f}]')
             step += 1
 
         network.eval()
-        accuracy = 0.0
-        mean_loss = 0.0
         with torch.no_grad():
-            for images, labels in val_loader:
+            accuracy = 0.0
+            loss = 0.0
+            for images, labels in valid_loader:
+                images, labels = images.to(device), labels.to(device)
                 y_hat = network(images)
-                mean_loss += loss_fn(y_hat, labels)
+                loss += loss_fn(y_hat, labels)
                 prediction = torch.max(y_hat, dim=1)[1]
                 accuracy += (prediction == labels).sum().item()
-            accuracy /= val_num
-            print(f'Epoch: [{epoch + 1} / {epochs}], loss: [{mean_loss} / {val_num}], accuracy: [{accuracy:.4f}]')
+            accuracy /= valid_num
+            print(f'Epoch: [{epoch + 1} / {args.epochs}], loss: [{loss} / {valid_num}:.4f], accuracy: [{accuracy:.4f}]')
             if accuracy > best_acc:
                 best_acc = accuracy
-                torch.save(network.state_dict(), './output/{}_{}'.format(epoch, accuracy))
+                print('Saving model to ./output/')
+                torch.save(network.state_dict(), './output/weights.ckpt')
     writer.close()
